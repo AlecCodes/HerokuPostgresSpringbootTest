74e852f0a25377c4aca8be003dd06f08a4d82fa6
Alec Hannaford <alec322@gmail.com>
2023-03-04 14:41:15 -0800

    added foodlogic

---
+++ src/main/java/com/example/springpostgres/logic/AccountLogic.java
@@ -0,0 +1,17 @@
+package com.example.springpostgres.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.springpostgres.repository.FoodRepository;
+import com.example.springpostgres.entity.Food;
+import java.util.List;
+
+@Service
+public class FoodLogic {
+    @Autowired
+    private FoodRepository foodRepository;
+    public List<Food> getAccounts(){
+        return foodRepository.findAll();
+    }
+
+}
