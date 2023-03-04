3998de8ea886aa33f3830d3a3c69d30ff7b8d15f
Alec Hannaford <alec322@gmail.com>
2023-03-04 14:33:07 -0800

    added repository for food

---
+++ src/main/java/com/example/springpostgres/repository/FoodRepository.java
@@ -0,0 +1,8 @@
+package com.example.springpostgres.repository;
+
+import com.example.springpostgres.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Integer> {
+
+}
